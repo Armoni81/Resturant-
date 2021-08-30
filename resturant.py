fb4c2008865842975bff397c4ca0b89efeec9dd3
Armoni Tigner <atigner2@student.gsu.edu>
2021-08-30 15:20:05 -0400

    added class for resturant

---
+++ resturant.py
@@ -0,0 +1,26 @@
+class Resturant: 
+     def __init__(self, menu=[], customer= {}, order, fulfilled =True):
+         self.menu= menu 
+         self.customer= customer 
+         self.order = order 
+         self.fulfilled = fulfilled
+    # 
+    
+        
+
+
+
+
+
+
+
+
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+
+# p1 = Person("John", 36)
+
+# print(p1.name)
+# print(p1.age)
