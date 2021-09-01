4545c686b8eeabdc74f9f9b2d320ce442bcd5aeb
Armoni81 <89092516+Armoni81@users.noreply.github.com>
2021-09-01 11:52:24 -0400

    Add files via upload

--- resturant.py
+++ resturant.py
@@ -1,15 +1,14 @@
-class Resturant: 
-     def __init__(self, menu=[], customer= {}, order, fulfilled =True):
-         self.menu= menu 
-         self.customer= customer 
-         self.order = order 
-         self.fulfilled = fulfilled
-    # 
-    
+
+
+
+# class Resturant: 
+#     def __init__(self, menu=[], customer_name= [], order=0):
+#         self.menu = menu 
+#         self.customer =customer 
+#         self.order = order
+
+#     def resturant_menu (self,menu):
         
-
-
-
 
 
 
@@ -20,7 +19,28 @@ class Resturant:
 #     self.name = name
 #     self.age = age
 
-# p1 = Person("John", 36)
+#   def myfunc(self):
+#     print("Hello my name is " + self.name)
 
-# print(p1.name)
-# print(p1.age)
+# p2 = Person("John", 36)
+# p2.myfunc()
+
+
+class Resturant:
+    def __init__(self, menu_dict={}, customer={} , wallet=100):
+        self.menu_dict= menu_dict
+        self.customer= customer 
+        self.wallet = wallet
+    def add_menu_items(self,add_new_items,menu):
+        self.menu_dict[menu_name]= menu
+    def remove_menu_items(self,menu_name,item_name):
+        del self.menu_dict[menu_name][menu_item] = menu 
+    def change_item_price(self,menu_name, new_price):
+        self.menu([menu_name],[items][item_name][price]):
+        
+
+
+  
+
+    
+
